99343fa00e8cfbfba25aa2c1ceebe3fab1d58bb9
Dimitra Sgourdou <dimitrasgou@gmail.com>
2022-07-10 21:13:41 +0300

    Created Entity Services - implemented base wiring

---
+++ src/Services/Menu.java
@@ -0,0 +1,19 @@
+package Services;
+
+public class MenuService {
+
+    public static void init()
+    {
+        System.out.println("__________*** M E N U ***___________");
+        System.out.println("====================================");
+        System.out.println("      What would you like to do?\n");
+        System.out.println("\nInsert-Edit-Delete Information of Theater Entities.Show..................1");
+        System.out.println("Insert-Edit-Delete Information of Music Entities.Show....................2");
+        System.out.println("Insert-Edit-Delete Information of Entities.Client........................3");
+        System.out.println("Book a ticket for a Theater Entities.Show................................4");
+        System.out.println("Book a ticket for a Music.. Entities.Show................................5");
+        System.out.println("View Statistics..........................................................6");
+        System.out.println("Exit the Menu............................................................7");
+        System.out.println("\n\nEnter your choice:...");
+    }
+}
